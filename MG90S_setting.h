23376adca123828f243c7f513e3ee27dc17efca2
zyx <2511305125@qq.com>
2025-03-31 22:19:22 +0100

    Settings for MG90S

---
+++ MG90S_setting.h
@@ -0,0 +1,33 @@
+#ifndef MG90S_SETTING_H
+#define MG90S_SETTING_H
+
+#include "pwm.h"
+
+class MG90S
+{
+    public:
+    void start_mg90s()
+    {
+        pwm.start_pwm(channel,frequency,7.5, chip);
+    }
+
+    void setAngle(int angle)
+    {
+        int duty_ns = angleToDutyNs(angle);
+        pwm.setDutyCycle(duty_ns);
+    }
+
+    private:
+    PWM pwm;
+    
+    const int channel = 2;
+    const int frequency = 50;
+    const int chip = 2;
+
+    int angleToDutyNs(int angle)
+    {
+        return 500000 + angle * (200000/180);
+    }
+};
+
+#endif
