68badea7161232b11516ea9a3963effe731dd3bb
zyx <2511305125@qq.com>
2025-04-10 15:22:53 +0100

    DEBUG

--- demo.cpp
+++ demo.cpp
@@ -4,13 +4,19 @@
 
 using namespace std;
 
-int main() {
-    PWM pwm;
-    pwm.start_pwm(2, 50, 7.5, 2); // chip=2, channel=2
+int main(int argc, char *argv[])
+{
+    MG90S mg90s;
+    mg90s.start_mg90s();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    cout << "1" << endl;
+    mg90s.setAngle(10);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    cout << "2"<< endl;
+    mg90s.setAngle(140);
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    pwm.setDutyCycle(2.5);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    pwm.setDutyCycle(12.5);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    cout << "Motor move fixed" << endl;
+
+    return 0;
 }
