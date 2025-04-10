d9df6e47184ec21b71050f403239feb3e879493f
zyx <2511305125@qq.com>
2025-04-10 14:38:45 +0100

    Debug

--- MG90S_setting.h
+++ MG90S_setting.h
@@ -6,7 +6,7 @@
 class MG90S
 {
     public:
-    PWM pwm;
+    
     void start_mg90s()
     {
         pwm.start_pwm(channel,frequency,7.5, chip);
@@ -20,9 +20,9 @@ class MG90S
     }
 
     private:
+    PWM pwm;
     
-    
-    const int channel = 2;
+    const int channel = 3;
     const int frequency = 50;
     const int chip = 2;
 
--- demo.cpp
+++ demo.cpp
@@ -7,17 +7,18 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     MG90S mg90s;
+    PWM pwm;
     mg90s.start_mg90s();
     std::this_thread::sleep_for(std::chrono::seconds(2));
     cout << "1" << endl;
-    mg90s.pwm.setDutyCycle(0);
+    pwm.setDutyCycle(0);
     std::this_thread::sleep_for(std::chrono::seconds(2));
     cout << "2"<< endl;
-    mg90s.pwm.setDutyCycle(7.5);
+    pwm.setDutyCycle(7.5);
     std::this_thread::sleep_for(std::chrono::seconds(2));
     cout << "3" << endl;
 
-    mg90s.pwm.setDutyCycle(15);
+    pwm.setDutyCycle(15);
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
     cout << "Motor move fixed" << endl;
