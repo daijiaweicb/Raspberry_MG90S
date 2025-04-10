160bf033ec99963efa44002e089e0eaae05ef2c4
zyx <2511305125@qq.com>
2025-04-10 14:50:07 +0100

    Debug

--- demo.cpp
+++ demo.cpp
@@ -7,21 +7,22 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     MG90S mg90s;
-    PWM pwm;
     mg90s.start_mg90s();
     std::this_thread::sleep_for(std::chrono::seconds(2));
+
     cout << "1" << endl;
-    pwm.setDutyCycle(0);
+    mg90s.setAngle(0); // 相当于 0 度，500us
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    cout << "2"<< endl;
-    pwm.setDutyCycle(7.5);
+
+    cout << "2" << endl;
+    mg90s.setAngle(90); // 中间位置，1500us
     std::this_thread::sleep_for(std::chrono::seconds(2));
+
     cout << "3" << endl;
-
-    pwm.setDutyCycle(15);
+    mg90s.setAngle(180); // 最大角度，2500us
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    cout << "Motor move fixed" << endl;
+    cout << "Motor moved" << endl;
 
     return 0;
 }
