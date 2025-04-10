7b0cebd207d0d9d37c58e90da40ec514e00971bd
zyx <2511305125@qq.com>
2025-04-10 15:33:24 +0100

    TEST

--- demo.cpp
+++ demo.cpp
@@ -9,17 +9,17 @@ int main(int argc, char *argv[])
     MG90S mg90s;
     mg90s.start_mg90s();
     // mg90s.setAngle(90);
-    cout << "reset" << endl;
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    cout << "1" << endl;
-    mg90s.setAngle(0);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    cout << "2"<< endl;
-    mg90s.setAngle(90);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    cout << "3"<< endl;
-    mg90s.setAngle(180);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    // cout << "reset" << endl;
+    // std::this_thread::sleep_for(std::chrono::seconds(2));
+    // cout << "1" << endl;
+    // mg90s.setAngle(0);
+    // std::this_thread::sleep_for(std::chrono::seconds(2));
+    // cout << "2"<< endl;
+    // mg90s.setAngle(90);
+    // std::this_thread::sleep_for(std::chrono::seconds(2));
+    // cout << "3"<< endl;
+    // mg90s.setAngle(180);
+    // std::this_thread::sleep_for(std::chrono::seconds(2));
 
     cout << "Motor move fixed" << endl;
 
