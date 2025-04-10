5fdb9a6d88e7b5ace8cf1219a9a82da4273a3f31
zyx <2511305125@qq.com>
2025-04-10 15:16:56 +0100

    DEBUG

--- demo.cpp
+++ demo.cpp
@@ -9,7 +9,7 @@ int main() {
     pwm.start_pwm(2, 50, 7.5, 2); // chip=2, channel=2
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    pwm.setDutyCycle(0);
+    pwm.setDutyCycle(1);
     std::this_thread::sleep_for(std::chrono::seconds(2));
     pwm.setDutyCycle(15);
     std::this_thread::sleep_for(std::chrono::seconds(2));
