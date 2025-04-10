ec5b12060ed605b601193a736ab8c5c85f155565
zyx <2511305125@qq.com>
2025-04-10 14:56:06 +0100

    DEBUG

--- demo.cpp
+++ demo.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main() {
     PWM pwm;
-    pwm.start_pwm(3, 50, 7.5, 0); // GPIO18, 50Hz, 中位
+    pwm.start_pwm(1, 50, 7.5, 0); // GPIO18, 50Hz, 中位
     std::this_thread::sleep_for(std::chrono::seconds(2));
     pwm.setDutyCycle(5);          // 5% duty = 左边
     std::this_thread::sleep_for(std::chrono::seconds(2));
